180cf7be274a8e46ee78706c951d7861f9f8998b
diegoavena <avena@chapman.edu>
2020-11-13 10:56:33 -0800

    Username.sh script completed

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Diego Avena
 echo "Enter a username: "
 read -r USERNAME
-while echo "$USERNAME" | grep -E -v "^[a-z][0-9]{3}$" > /dev/null 2>&1
+while echo "$USERNAME" | grep -E  -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username that follows these rules: "
 	echo "Rule 1: Contains at least 3 but no more than 12 characters"
