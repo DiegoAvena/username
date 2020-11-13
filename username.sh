8c49a2199af15235fa52ce1eecc646c490674059
diegoavena <avena@chapman.edu>
2020-11-13 10:36:16 -0800

    Removed zip files, and added in username.sh file

---
+++ username.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# username.sh
+# Diego Avena
+echo "Enter a username: "
+read -r USERNAME
+while echo "$USERNAME" | grep -E -v "^[a-z][0-9]{3}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username that follows these rules: "
+	echo "Rule 1: Contains at least 3 but no more than 12 characters"
+	echo "Rule 2: Contains only lower case letters, digits, and the underscore character"
+	echo "Rule 3: Starts with a lower case letter"
+	echo "Enter a username: "
+	read -r USERNAME
+done
+echo "Thank you"
